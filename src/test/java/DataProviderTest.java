e28730382882b8ba561dc1d6795711d541582493
MaiaGelashvili <116916210+MaiaGelashvili@users.noreply.github.com>
2023-03-03 11:42:07 +0400

    Update DataProviderTest.java

--- src/test/java/DataProviderTest.java
+++ src/test/java/DataProviderTest.java
@@ -7,7 +7,7 @@ public class DataProviderTest {
 
         @DataProvider
         public Object[][] dpMethod(){
-            return new Object[][] {{2, 3 , 7}, {5, 2, 9}};
+            return new Object[][] {{2, 3 , 7}, {5, 2, 1}};
         }
 
         @Test(dataProvider = "dpMethod")
